a960cb5ea86d6a31189275959d3a4fcb7d11d118
Zander Work <zander@zanderwork.com>
2019-06-19 18:26:52 -0400

    added sample config

---
+++ config.sample.py
@@ -0,0 +1,14 @@
+SERVER_ID = 1234
+VERIFIED_ROLE_ID = 1234
+ADMIN_ROLE_ID = 1234
+
+VALID_ROLES = [{"name": "role1", "id": 1234}, {"name": "role2", "id": 1234}] # can add more
+
+DISCORD_CLIENT_TOKEN = 'tokentokentoken'
+
+EMAIL_SERVER = "something.com"
+EMAIL_USERNAME = "something@example.com"
+EMAIL_PASSWORD = "hunter2"
+
+def generate_token(member):
+    return "some function here"
