c9a3af7bb7b9c33d33a303a42e0a8fadf6c15831
Zander Work <zander@zanderwork.com>
2019-07-14 08:37:16 -0400

    updated config

--- config.sample.py
+++ config.sample.py
@@ -2,6 +2,11 @@ SERVER_ID = 1234
 VERIFIED_ROLE_ID = 1234
 ADMIN_ROLE_ID = 1234
 
+EMBED_DEFAULT = 0x46b7e0 # blue
+EMBED_SUCCESS = 0x10e82d # green
+EMBED_WARNING = 0xd6e810 # yellow
+EMBED_ERROR = 0xe81010 # red
+
 VALID_ROLES = {"role1": 1234, "role2": 1234}
 
 DISCORD_CLIENT_TOKEN = 'tokentokentoken'
@@ -9,6 +14,8 @@ DISCORD_CLIENT_TOKEN = 'tokentokentoken'
 EMAIL_SERVER = "something.com"
 EMAIL_USERNAME = "something@example.com"
 EMAIL_PASSWORD = "hunter2"
+VALID_EMAIL_DOMAINS = ["example.com"]
 
-def generate_token(member):
-    return "some function here"
+# Below are set dynamically
+server = None
+verified_role = None
