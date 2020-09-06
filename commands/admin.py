1a2f4c0c54e2d7db4c6c597b8825d0d07de2faea
Robert Detjens <detjensrobert@gmail.com>
2020-09-05 22:28:00 -0700

    fix logging

--- commands/admin.py
+++ commands/admin.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from util.checks import *
 from util.func import *
 
+logger = logging.getLogger("discord")
 
 class AdminCommands(commands.Cog):
     def __init__(self, bot):
